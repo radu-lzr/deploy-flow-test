5c545408847c41f11a47d150a7f7b517c62bde83
Radu Lazar <radu.lazar@cellenza.com>
2026-03-10 10:02:10 +0100

    chore: deploy terraform for poc [DeployFlow]

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -12,14 +12,22 @@ resource "azurerm_kubernetes_cluster" "aks" {
     auto_scaling_enabled        = true
     min_count                   = var.aks_node_pool_min_count
     max_count                   = var.aks_node_pool_max_count
+    os_disk_size_gb             = var.aks_os_disk_size_gb
     temporary_name_for_rotation = "temppool"
+    node_labels                 = {
+      "workload" = "odoo"
+    }
     tags                        = var.tags
+    vm_size                     = var.aks_node_vm_size
   }
   identity {
     type = "SystemAssigned"
   }
   network_profile {
     network_plugin    = var.aks_network_plugin
+    network_policy    = var.aks_network_policy
+    service_cidr      = var.aks_service_cidr
+    dns_service_ip    = var.aks_dns_service_ip
     load_balancer_sku = var.aks_load_balancer_sku
   }
 

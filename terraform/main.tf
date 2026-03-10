474639ba8fc5472d3a5a40a66a96e356927f75fe
Radu Lazar <radu.lazar@cellenza.com>
2026-03-10 11:35:40 +0100

    chore: deploy terraform for poc [DeployFlow]

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "resource_group" {
+  location = "northeurope"
+  name     = "rg-deployflow-poc"
+}
+
+resource "azurerm_service_plan" "service_plan" {
+  location            = azurerm_resource_group.resource_group.location
+  name                = "asp-deployflow-poc"
+  os_type             = "Linux"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  sku_name            = "B1"
+}
